cec289bdaff9e32ce04fd2947ac599d90416a21e
vduong <vthanhd@gmail.com>
2023-03-01 12:34:22 -0800

    changed MONGODB

--- server/models/models.js
+++ server/models/models.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.API_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
   console.log('Connected to Database');
 });
