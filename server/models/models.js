9566a797911adf75674714c4e88926cabdaf232b
vduong <vthanhd@gmail.com>
2023-03-01 11:55:45 -0800

    configured .env

--- server/models/models.js
+++ server/models/models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-;
+require('dotenv').config();
 
 mongoose.connect(process.env.API_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
